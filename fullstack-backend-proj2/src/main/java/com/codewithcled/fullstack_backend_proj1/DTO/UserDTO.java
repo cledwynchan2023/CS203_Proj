5cb1b32da0a25ddba70800a9621f8e113e40f916
ChewWaiMunDamien <damien.chew.2023@smu.edu.sg>
2024-09-30 00:38:28 +0800

    Add files via upload

---
+++ fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/DTO/UserDTO.java
@@ -0,0 +1,39 @@
+package com.codewithcled.fullstack_backend_proj1.DTO;
+
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+    private Integer elo;
+
+    public UserDTO(Long id,String username,String email,String role,Integer elo){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.role=role;
+        this.elo=elo;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getUserName(){
+        return username;
+    }
+
+    public Integer getElo() {
+        return elo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
+
+
