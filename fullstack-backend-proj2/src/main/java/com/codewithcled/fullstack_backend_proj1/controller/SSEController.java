96dc2bd644f7e43c65d62e1b61045e9f762f7757
chikenkurry <151176393+chikenkurry@users.noreply.github.com>
2024-10-09 17:33:20 +0800

    updated sse

--- fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/controller/SSEController.java
+++ fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/controller/SSEController.java
@@ -11,14 +11,15 @@ import com.codewithcled.fullstack_backend_proj1.DTO.UserDTO;
 import com.codewithcled.fullstack_backend_proj1.service.UserServiceImplementation;
 import com.codewithcled.fullstack_backend_proj1.service.TournamentService;
 import reactor.core.publisher.Flux;
+
+import java.util.Collections;
 import java.util.List;
 import java.time.Duration;
-
 
 @RestController
 @RequestMapping("/update")
 public class SSEController {
-    
+
     @Autowired
     UserServiceImplementation userService;
 
@@ -28,29 +29,29 @@ public class SSEController {
     private final int timer = 5;
 
     @CrossOrigin(allowedHeaders = "*")
-    @GetMapping(value="/sse/users",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/sse/users", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<List<UserDTO>> getMethodName() {
         return Flux.interval(Duration.ofSeconds(timer)).map(tick -> userService.findAllUsersDTO());
     }
 
     @GetMapping(value = "/sse/user", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public List<UserDTO> getUserDTOS(){
+    public List<UserDTO> getUserDTOS() {
         return userService.findAllUsersDTO();
     }
 
-    // @GetMapping(value = "/sse/tournament", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    // public Flux<List<TournamentDTO>> getTournament() {
-    //     return Flux.interval(Duration.ofSeconds(timer))
-    //             .map(tick -> {
-    //                 try {
-    //                     return tournamentService.findAllTournamentsDTO();
-    //                 } catch (Exception e) {
-    //                     // Log the exception and return an empty list
-    //                     System.out.println("Error occurred: " + e.getMessage());
-                    
-    //                     return Collections.emptyList();
-    //                 }
-    //             });
-    // }
-    
+    @GetMapping(value = "/sse/tournament", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<List<TournamentDTO>> getTournament() {
+        return Flux.interval(Duration.ofSeconds(timer))
+                .map(tick -> {
+                    try {
+                        return tournamentService.findAllTournamentsDTO();
+                    } catch (Exception e) {
+                        // Log the exception and return an empty list
+                        System.out.println("Error occurred: " + e.getMessage());
+
+                        return Collections.emptyList();
+                    }
+                });
+    }
+
 }
