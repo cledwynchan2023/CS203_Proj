5c8c5cc33b5eaa1d8cc0d62d654cf07aea19f4b9
chikenkurry <151176393+chikenkurry@users.noreply.github.com>
2024-10-09 17:37:22 +0800

    Create EloRatingService.java

---
+++ fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/service/EloRatingService.java
@@ -0,0 +1,12 @@
+package com.codewithcled.fullstack_backend_proj1.service;
+
+public interface EloRatingService {
+
+    public double WinProbabilityOnElo(int elo1,int elo2);
+    
+    public double EloCalculation(int elo1,int elo2,int outcome);
+
+    public double WinValue(int outcome);
+
+    public int getKValue(int eloScore);
+}
