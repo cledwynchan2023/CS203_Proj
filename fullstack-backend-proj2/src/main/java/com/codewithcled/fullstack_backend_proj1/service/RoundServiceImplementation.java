29d348dac89121c0dbf91b944373ec98de661876
Zern <zern.chng.2023@scis.smu.edu.sg>
2024-11-05 12:30:22 +0800

    sorts roundscoreboard automatically on match update
    creating new round copies previous round results

--- fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/service/RoundServiceImplementation.java
+++ fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/service/RoundServiceImplementation.java
@@ -285,6 +285,7 @@ public class RoundServiceImplementation implements RoundService {
         //and to set up matches
         Map<Long, Double> prevRoundScoreboard = tournament.getRounds().get(tournament.getRounds().size() - 1).getScoreboard();
         List<Entry<Long, Double>> prevRoundScoreboardList = new ArrayList<>(prevRoundScoreboard.entrySet());
+        newRound.setScoreboard(prevRoundScoreboard);
         
         //pair up participants by score
         List<Match> matches = new ArrayList<>();
