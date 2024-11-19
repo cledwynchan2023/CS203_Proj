f07d46a794be69b9290772d5c80ed5cb00604f09
shyannnnn <jslim.2023@scis.smu.edu.sg>
2024-11-20 00:23:35 +0800

    deployed

--- fullstack-backend-proj2/src/main/java/com/codewithcled/fullstack_backend_proj1/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.codewithcled.fullstack_backend_proj1.config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY = "wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
-    public static final String JWT_HEADER = "Authorization";
-}
